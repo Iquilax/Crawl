08193805cbfbfb9947705aa05be7a4251a36be3b
ThienPG <giathienpham@gmail.com>
2017-03-23 00:38:18 +0700

    add ProductDetailActivity, utility for Glide, Logger and MessageString, adapter folder

---
+++ app/src/main/java/com/iwlac/tracky/utility/MessageString.java
@@ -0,0 +1,11 @@
+package com.iwlac.tracky.utility;
+
+/**
+ * Created by Thien on 3/23/2017.
+ */
+
+public class MessageString {
+    public static final String UNEXPECTED_ERROR = "Unexpected Error Happen! Please try again";
+
+    public static final String TRACK_SUCCESSFULLY = "Track successfully";
+}
