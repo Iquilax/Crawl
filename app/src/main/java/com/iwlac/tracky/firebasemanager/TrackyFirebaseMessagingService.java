40fb27c87d879e54afdcf144045659f7f503fa5f
ThienPG <giathienpham@gmail.com>
2017-03-26 23:41:50 +0700

    SHOW BADGE NUMBER

--- app/src/main/java/com/iwlac/tracky/firebasemanager/TrackyFirebaseMessagingService.java
+++ app/src/main/java/com/iwlac/tracky/firebasemanager/TrackyFirebaseMessagingService.java
@@ -10,10 +10,16 @@ import android.net.Uri;
 import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.github.arturogutierrez.Badges;
+import com.github.arturogutierrez.BadgesNotSupportedException;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
 import com.iwlac.tracky.activity.MainActivity;
 import com.iwlac.tracky.R;
+import com.iwlac.tracky.activity.ProductDetailActivity;
+
+import static com.iwlac.tracky.utility.IntentConstant.EXTRA_PRODUCT_NAME;
+
 
 /**
  * Created by Thien on 3/21/2017.
@@ -39,6 +45,14 @@ public class TrackyFirebaseMessagingService extends FirebaseMessagingService {
             Log.d(TAG, "Message Notification Body: " + remoteMessage.getNotification().getBody());
             sendNotification(remoteMessage.getNotification().getBody());
         }
+
+        //Set badge number
+        try {
+            Log.d(TAG, "Setting number on app icon");
+            Badges.setBadge(this, 5);
+        } catch (BadgesNotSupportedException badgesNotSupportedException) {
+            Log.d(TAG, badgesNotSupportedException.getMessage());
+        }
     }
 
     @Override
@@ -52,7 +66,8 @@ public class TrackyFirebaseMessagingService extends FirebaseMessagingService {
     }
 
     private void sendNotification(String messageBody) {
-        Intent intent = new Intent(this, MainActivity.class);
+        Intent intent = new Intent(this, ProductDetailActivity.class);
+        intent.putExtra(EXTRA_PRODUCT_NAME, "");
         intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP);
         PendingIntent pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
                 PendingIntent.FLAG_ONE_SHOT);
---
+++ app/src/main/java/com/iwlac/tracky/utility/IntentConstant.java
@@ -0,0 +1,11 @@
+package com.iwlac.tracky.utility;
+
+/**
+ * Created by Thien on 3/26/2017.
+ */
+
+public class IntentConstant {
+
+    public static String EXTRA_PRODUCT_NAME = "ProductName";
+    public static String EXTRA_PRODUCT_PRICE = "ProductPrice";
+}
